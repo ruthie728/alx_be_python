deb431ea9bba37371b889feb95e0660521750213
Ruth Atieno <atienoruth479@gmail.com>
2025-09-13 22:02:07 +0300

    Add finance calculator script

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,15 @@
+# finance_calculator.py
+
+# User input
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+
+# Calculate monthly savings
+monthly_savings = monthly_income - monthly_expenses
+
+# Project annual savings with 5% interest
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+# Output results
+print(f"Your monthly savings are ${monthly_savings}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
