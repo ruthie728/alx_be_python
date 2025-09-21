d3d81b992221064cfdc9467c17a25cafac3e6e4c
Ruth Atieno <atienoruth479@gmail.com>
2025-09-21 20:36:38 +0300

    change

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,3 +1,5 @@
+# match_case_calculator.py
+
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operator = input("Choose the operation (+, -, *, /): ")
@@ -14,8 +16,6 @@ match operator:
         if num2 == 0:
             print("Cannot divide by zero.")
         else:
-            
             print(f"Result: {num1 / num2}")
-            
     case _:
         print("Invalid operator")
